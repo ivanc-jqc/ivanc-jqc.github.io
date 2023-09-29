3adf035605a88e08aabff460117ed9f5d3d2b9ce
ivan on laptop <ivanc.jqc@gmail.com>
2023-09-30 00:45:33 +0100

    added files

---
+++ cc_tbp_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start(): void;
+export function _web_worker_entry_point(a: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h888a797e98f2ebc9(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut__h589de3f251b5b786(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf5c6de645f2c40e9(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
